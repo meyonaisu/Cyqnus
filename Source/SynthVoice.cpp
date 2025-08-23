b18406016fc9eb74081dfe42a75cd9d9d1d0ef3e
zink_urq00 <iamklarxz00@gmail.com>
2025-08-23 23:54:53 +0700

    Adding oscillators to SynthVoice

--- Source/SynthVoice.cpp
+++ Source/SynthVoice.cpp
@@ -7,11 +7,36 @@ SynthVoice::SynthVoice(juce::AudioProcessorValueTreeState& state)
 	pDecay   = apvts.getRawParameterValue("ampDecay");
 	pSustain = apvts.getRawParameterValue("ampSustain");
 	pRelease = apvts.getRawParameterValue("ampRelease");
+
+	pOsc1Wave = apvts.getRawParameterValue("osc1Wave");
+	pOsc1Level = apvts.getRawParameterValue("osc1Level");
+	pOsc1Coarse = apvts.getRawParameterValue("osc1Coarse");
+	pOsc1Fine = apvts.getRawParameterValue("osc1Fine");
+	pOsc1PW = apvts.getRawParameterValue("osc1PW");
+	pOsc1Detune = apvts.getRawParameterValue("osc1Detune");
+
+	pOsc2Wave = apvts.getRawParameterValue("osc2Wave");
+	pOsc2Level = apvts.getRawParameterValue("osc2Level");
+	pOsc2Coarse = apvts.getRawParameterValue("osc2Coarse");
+	pOsc2Fine = apvts.getRawParameterValue("osc2Fine");
+	pOsc2PW = apvts.getRawParameterValue("osc2PW");
+	pOsc2Detune = apvts.getRawParameterValue("osc2Detune");
+
+	pOsc3Wave = apvts.getRawParameterValue("osc3Wave");
+	pOsc3Level = apvts.getRawParameterValue("osc3Level");
+	pOsc3Coarse = apvts.getRawParameterValue("osc3Coarse");
+	pOsc3Fine = apvts.getRawParameterValue("osc3Fine");
+	pOsc3PW = apvts.getRawParameterValue("osc3PW");
+	pOsc3Detune = apvts.getRawParameterValue("osc3Detune");
 }
 
 void SynthVoice::prepareToPlay(double sampleRate, int) {
 	this->sampleRate = (sampleRate > 0.0) ? sampleRate : 44100.0;
 	ampEnv.setSampleRate(sampleRate);
+
+	osc1.setSampleRate(sampleRate);
+	osc2.setSampleRate(sampleRate);
+	osc3.setSampleRate(sampleRate);
 }
 
 bool SynthVoice::canPlaySound(juce::SynthesiserSound* sound) {
@@ -31,6 +56,27 @@ void SynthVoice::startNote(int midiNoteNumber, float velocity, juce::Synthesiser
 	envParams.release = pRelease->load();
 	ampEnv.setParameters(envParams);
 	ampEnv.noteOn();
+
+	auto configureOsc = [this](Oscillator& osc,
+		std::atomic<float>* wave,
+		std::atomic<float>* level,
+		std::atomic<float>* coarse,
+		std::atomic<float>* fine,
+		std::atomic<float>* pw,
+		std::atomic<float>* detune)
+		{
+			osc.setWaveform(static_cast<Oscillator::Waveform>(static_cast<int>(wave->load())));
+			osc.setLevel(level->load());
+			osc.setCoarse(static_cast<int>(coarse->load()));
+			osc.setFinetine(fine->load());
+			osc.setPulseWidth(pw->load());
+			osc.setDetuneSpread(detune->load());
+			osc.setFrequency(currentFreq);
+		};
+
+	configureOsc(osc1, pOsc1Wave, pOsc1Level, pOsc1Coarse, pOsc1Fine, pOsc1PW, pOsc1Detune);
+	configureOsc(osc2, pOsc2Wave, pOsc2Level, pOsc2Coarse, pOsc2Fine, pOsc2PW, pOsc2Detune);
+	configureOsc(osc3, pOsc3Wave, pOsc3Level, pOsc3Coarse, pOsc3Fine, pOsc3PW, pOsc3Detune);
 }
 
 void SynthVoice::stopNote(float, bool allowTailOff) {
@@ -45,8 +91,10 @@ void SynthVoice::stopNote(float, bool allowTailOff) {
 void SynthVoice::pitchWheelMoved(int) {}
 void SynthVoice::controllerMoved(int, int) {}
 
-void SynthVoice::renderNextBlock(juce::AudioBuffer<float>& output, int startSample, int numSamples) {
-	if (!ampEnv.isActive()) {
+void SynthVoice::renderNextBlock(juce::AudioBuffer<float>& output, int startSample, int numSamples)
+{
+	if (!ampEnv.isActive())
+	{
 		clearCurrentNote();
 		return;
 	}
@@ -54,21 +102,16 @@ void SynthVoice::renderNextBlock(juce::AudioBuffer<float>& output, int startSamp
 	auto* left = output.getWritePointer(0, startSample);
 	auto* right = output.getNumChannels() > 1 ? output.getWritePointer(1, startSample) : nullptr;
 
-	const float twoPiOverSR = 2.0f * static_cast<float>(juce::MathConstants<double>::pi) / static_cast<float>(getSampleRate());
-	const float phaseInc = currentFreq * twoPiOverSR;
+	for (int i = 0; i < numSamples; ++i)
+	{
+		float oscMix = (osc1.getNextSample() + osc2.getNextSample() + osc3.getNextSample()) / 3.0f;
 
-	for (int i = 0; i < numSamples; ++i) {
-		const float osc = std::sin(phase);
-		phase += phaseInc;
-		if (phase >= juce::MathConstants<float>::twoPi)
-			phase -= juce::MathConstants<float>::twoPi;
-
-		const float env = ampEnv.getNextSample();
-		const float sample = osc * env * level;
+		float env = ampEnv.getNextSample();
+		float sample = oscMix * env * level;
 
 		left[i] += sample;
 		if (right) right[i] += sample;
-	} 
+	}
 
 	if (!ampEnv.isActive())
 		clearCurrentNote();
--- Source/SynthVoice.h
+++ Source/SynthVoice.h
@@ -2,6 +2,7 @@
 #include <JuceHeader.h>
 #include "AHDSR.h"
 #include "SynthSound.h"
+#include "Oscillator.h"
 
 class SynthVoice : public juce::SynthesiserVoice { 
 public:
@@ -18,13 +19,36 @@ public:
 private:
 	juce::AudioProcessorValueTreeState& apvts;
 
+	AHDSR ampEnv;
+	Oscillator osc1, osc2, osc3;
+
 	std::atomic<float>* pAttack{ nullptr };
 	std::atomic<float>* pHold{ nullptr };
 	std::atomic<float>* pDecay{ nullptr };
 	std::atomic<float>* pSustain{ nullptr };
 	std::atomic<float>* pRelease{ nullptr };
 
-	AHDSR ampEnv;
+	std::atomic<float>* pOsc1Wave{ nullptr };
+	std::atomic<float>* pOsc1Level{ nullptr };
+	std::atomic<float>* pOsc1Coarse{ nullptr };
+	std::atomic<float>* pOsc1Fine{ nullptr };
+	std::atomic<float>* pOsc1PW{ nullptr };
+	std::atomic<float>* pOsc1Detune{ nullptr };
+
+	std::atomic<float>* pOsc2Wave{ nullptr };
+	std::atomic<float>* pOsc2Level{ nullptr };
+	std::atomic<float>* pOsc2Coarse{ nullptr };
+	std::atomic<float>* pOsc2Fine{ nullptr };
+	std::atomic<float>* pOsc2PW{ nullptr };
+	std::atomic<float>* pOsc2Detune{ nullptr };
+
+	std::atomic<float>* pOsc3Wave{ nullptr }; 
+	std::atomic<float>* pOsc3Level{ nullptr };
+	std::atomic<float>* pOsc3Coarse{ nullptr };
+	std::atomic<float>* pOsc3Fine{ nullptr };
+	std::atomic<float>* pOsc3PW{ nullptr };
+	std::atomic<float>* pOsc3Detune{ nullptr };
+
 
 	double sampleRate = 44100.0;
 	float  currentFreq = 440.0f;
